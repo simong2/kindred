6d84e8fd27649465d3a60947baeef74e1738a612
sandra2j <sandra2jose@gmail.com>
2025-02-09 06:13:23 -0500

    added comments to colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,7 +1,14 @@
 class Colors {
-  // AppBar Color
+
+  // medium spaces
   static const mediumGreen = 0xFFA3B899;
-  static const darkGreen = 0xFF667B68;
-  static const lightGreen = 0xFFDDE6D5;
+
+  // font/fine lines/borders/logos/small spaces
+  static const darkGreenFont = 0xFF667B68;
+
+  // background/large spaces
+  static const lightGreenBackGround = 0xFFDDE6D5;
+
+  //fairy
   static const mellowYellow = 0xFFE4C99E;
 }
